afd521c2b5a4d78f9e73789f529239c9cb6cac78
Timo J. Rinne <tri@iki.fi>
2019-11-11 14:56:48 +0000

    Added tests for promise versions.

--- test/test.js
+++ test/test.js
@@ -7,47 +7,73 @@ const ReadableTestDataStream = require('readable-test-data-stream');
 const tv = require('unix-checksum/test/testvectors.js');
 const ht = require('./hashtests.js');
 const UnixChecksumStream = require('../index.js');
+const ph = require('../promise.js');
 
 var tests = { running: 0,
 			  completed: {} };
+
 (function() {
-	tv.forEach(function(v) {
-		[ 'bsdsum', 'sysvsum', 'cksum', 'crc32', 'crc32c' ].forEach(function (a) {
+	var algs = UnixChecksumStream.algorithms();
+	tv.concat(ht).forEach(function(v) {
+		Object.keys(v).filter((k) => (algs.indexOf(k) >= 0)).forEach(function (a) {
 			var i = v.input;
 			var c = v[a];
 			var s = new UnixChecksumStream(a);
 			var d = new ReadableTestDataStream(v.input);
 			s.on('digest', function(digest) {
-				if (typeof(c) === 'number') {
-                    assert.equal(c, digest, 'Checksum error in ' + a);
-					tests.completed[a] = (tests.completed[a] ? tests.completed[a] + 1 : 1);
-				}
-				tests.running--;
-			});
-			tests.running++;
-			d.pipe(s);
-		});
-	});
-	ht.forEach(function(v) {
-		[ 'md5', 'sha1', 'sha256', 'sha512' ].forEach(function (a) {
-			var i = v.input;
-			var c = v[a];
-			var s = new UnixChecksumStream(a);
-			var d = new ReadableTestDataStream(v.input);
-			s.on('digest', function(digest) {
-				digest = s.digest('hex');
 				if (typeof(c) === 'string') {
-                    assert.equal(c, digest, 'Checksum error in ' + a);
-					tests.completed[a] = (tests.completed[a] ? tests.completed[a] + 1 : 1);
+                    assert.equal(s.digest('hex'), c, 'Checksum error in ' + a);
+				} else if (typeof(c) === 'number') {
+                    assert.equal(s.digest('number'), c, 'Checksum error in ' + a);
+				} else if (Buffer.isBuffer(c)) {
+                    assert.ok(c.equals(s.digest('buffer')), 'Checksum error in ' + a);
 				}
+				tests.completed[a] = (tests.completed[a] ? tests.completed[a] + 1 : 1);
 				tests.running--;
 			});
 			tests.running++;
 			d.pipe(s);
+			(Promise.resolve()
+			 .then(function() {
+				 var e;
+				 if (typeof(c) === 'string') {
+					 e = 'hex';
+				 } else if (typeof(c) === 'number') {
+					 e = 'number';
+				 } else if (Buffer.isBuffer(c)) {
+                     e = 'buffer';
+				 }
+				 tests.running++;
+				 return ph.data(testChop(i), a, e);
+			 })
+			 .then(function(ret) {
+				 if (Buffer.isBuffer(ret)) {
+                     assert.ok(ret.equals(c), 'Checksum error in ' + a);
+				 } else {
+                     assert.equal(ret, c, 'Checksum error in ' + a);
+				 }
+				 tests.completed[a] = (tests.completed[a] ? tests.completed[a] + 1 : 1);
+				 tests.running--;
+			 })
+			 .catch(function(e) {
+				 console.error(e);
+				 process.exit(1);
+			 }));
 		});
 	});
 })();
 
+// Chop a string or buffer to array of chunks in arbitrary but still
+// deterministic way.
+function testChop(b) {
+	var r = [];
+	while (b.length > 0) {
+		let l = Math.ceil(b.length / 2);
+		r.push(b.slice(0, l));
+		b = b.slice(l);
+	}
+	return r;
+};
 
 var first = true;
 function ready() {
